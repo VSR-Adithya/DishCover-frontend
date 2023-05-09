7be68ba10c2dc0ccbb5a0ab1e9713d35e051b9b7
Vadapalli Sreerama Adithya <adithya.vadapalli2604@gmail.com>
2023-05-09 23:57:57 +0530

    corrected Routes

--- src/pages/AnimatedRoutes.js
+++ src/pages/AnimatedRoutes.js
@@ -11,6 +11,7 @@ import Recipe from './recipe';
 import Healthdash from "./healthdash";
 
 import { AnimatePresence } from "framer-motion";
+import { domain } from "../App";
 function AnimatedRoutes() {
   const location = useLocation();
 
@@ -18,14 +19,14 @@ function AnimatedRoutes() {
     <AnimatePresence mode='wait'>
       <Routes key={location.pathname} location={location}>
       <Route path="/" element={<Signin />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="signin" element={<Signin />} />
-          <Route path="saved" element={<Saved />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="Home" element={<Maindash />} />
-          <Route path="Health" element={<Healthdash />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="recipe" element={<Recipe />} />
+          <Route path={`${domain}/signup`} element={<Signup />} />
+          <Route path={`${domain}/signin`} element={<Signin />} />
+          <Route path={`${domain}/saved`} element={<Saved />} />
+          <Route path={`${domain}/dashboard`} element={<Dashboard />} />
+          <Route path={`${domain}/Home`} element={<Maindash />} />
+          <Route path={`${domain}/Health`} element={<Healthdash />} />
+          <Route path={`${domain}/profile`} element={<Profile />} />
+          <Route path={`${domain}/recipe`} element={<Recipe />} />
         </Routes>
     </AnimatePresence>
   );
